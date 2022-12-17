bae5e4daadcb3470035c01f09a734d238213e9d5
joanacabralmartins <joanacabralmartins@outlook.com>
2022-12-17 18:45:37 -0300

    criação das tabelas usuário e carona

--- sqls/script.sql
+++ sqls/script.sql
@@ -1,3 +1,16 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    id INTEGER PRIMARY KEY, 
+    cpf TEXT NOT NULL,
+    nome TEXT NOT NULL,
+    funcao_IFPR TEXT NOT NULL,
+    senha TEXT NOT NULL,
+    data_nascimento TEXT NOT NULL,
+    idade INTEGER NOT NULL,
+    curso TEXT NULL,
+    telefone TEXT NULL,
+    endereco TEXT NULL
+);
+
 CREATE TABLE IF NOT EXISTS motorista (
     id INTEGER PRIMARY KEY, 
     cpf TEXT NOT NULL,
@@ -18,5 +31,14 @@ CREATE TABLE IF NOT EXISTS carro (
     modelo TEXT NOT NULL,
     cor TEXT NOT NULL,
     cpf_motorista TEXT NOT NULL,
-    CONSTRAINT carro_FK_motorista FOREIGN KEY (id) REFERENCES motorista(id)
+    CONSTRAINT carro_FK_motorista FOREIGN KEY(cpf_motorista) REFERENCES motorista(cpf)
+);
+
+CREATE TABLE IF NOT EXISTS carona (
+    id INTEGER PRIMARY KEY, 
+    placa TEXT NOT NULL,
+    modelo TEXT NOT NULL, 
+    cor TEXT NOT NULL, 
+    cpf_motorista TEXT NOT NULL,
+    CONSTRAINT carona_FK_motorista FOREIGN KEY(cpf_motorista) REFERENCES motorista(cpf)
 );
