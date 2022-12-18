ca8367119778d857dc9d84052b0df65b6b364d86
joanacabralmartins <joanacabralmartins@outlook.com>
2022-12-18 02:51:27 -0300

    ajustes

--- src/main/java/ifpr/pgua/eic/projetointegrador/controllers/JanelaOpcaoCadastro.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/controllers/JanelaOpcaoCadastro.java
@@ -1,5 +1,19 @@
 package ifpr.pgua.eic.projetointegrador.controllers;
 
+import ifpr.pgua.eic.projetointegrador.App;
+import ifpr.pgua.eic.projetointegrador.utils.BorderPaneRegion;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class JanelaOpcaoCadastro {
-    
+
+    @FXML
+    private void carregaTelaCadastroUsuario(ActionEvent evento) {
+        App.changeScreenRegion("CADASTRO USUARIO", BorderPaneRegion.CENTER);
+    }
+
+    @FXML
+    private void carregaTelaCadastroMotorista(ActionEvent evento) {
+        App.changeScreenRegion("CADASTRO MOTORISTA", BorderPaneRegion.CENTER);
+    }
 }
