796849753bcabdc93c75980d5f58cfbbaff23bf0
joanacabralmartins <joanacabralmartins@outlook.com>
2022-12-18 09:11:22 -0300

    DAO's de usuário

---
+++ src/main/java/ifpr/pgua/eic/projetointegrador/models/daos/JDBCUsuarioDAO.java
@@ -0,0 +1,14 @@
+package ifpr.pgua.eic.projetointegrador.models.daos;
+
+import ifpr.pgua.eic.projetointegrador.models.entities.Usuario;
+import ifpr.pgua.eic.projetointegrador.models.results.Result;
+
+public class JDBCUsuarioDAO implements UsuarioDAO{
+
+    @Override
+    public Result create(Usuario caroneiro) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ src/main/java/ifpr/pgua/eic/projetointegrador/models/daos/UsuarioDAO.java
@@ -0,0 +1,8 @@
+package ifpr.pgua.eic.projetointegrador.models.daos;
+
+import ifpr.pgua.eic.projetointegrador.models.entities.Usuario;
+import ifpr.pgua.eic.projetointegrador.models.results.Result;
+
+public interface UsuarioDAO {
+    Result create(Usuario caroneiro);
+}
