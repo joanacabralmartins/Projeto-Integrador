61802945b3faff0c0cbef6b8ac904184b61f5bc3
joanacabralmartins <joanacabralmartins@outlook.com>
2023-01-01 23:08:39 -0300

    verifica se cpf do usuário não está cadastrado

--- src/main/java/ifpr/pgua/eic/projetointegrador/models/repositories/UsuarioRepository.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/models/repositories/UsuarioRepository.java
@@ -1,22 +1,32 @@
 package ifpr.pgua.eic.projetointegrador.models.repositories;
 
 import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
 
 import ifpr.pgua.eic.projetointegrador.models.daos.UsuarioDAO;
 import ifpr.pgua.eic.projetointegrador.models.entities.Usuario;
 import ifpr.pgua.eic.projetointegrador.models.results.Result;
 
 public class UsuarioRepository {
+    private List<Usuario> usuarios;
     private UsuarioDAO dao;
 
     public UsuarioRepository(UsuarioDAO dao) {
         this.dao = dao;
+
+        usuarios = dao.listAll();
     }
 
     public Result adicionarUsuario(String cpf, String nome, String funcao, String senha, Date dataNascimento,
                                     int idade, String curso, String telefone, String endereco) {
         Usuario caroneiro = new Usuario(cpf, nome, funcao, senha, dataNascimento, idade, curso, telefone, endereco);
         
+        Optional<Usuario> busca = usuarios.stream().filter((users)->users.getCpf().equals(cpf)).findFirst();
+        if (busca.isPresent()) { //verifica se o caroneiro realmente ainda não tem cadastro
+            return Result.fail("Usuário já cadastrado!");
+        }
+
         if (cpf.length() < 11) {
             return Result.fail("Insira um CPF válido!");
         }
