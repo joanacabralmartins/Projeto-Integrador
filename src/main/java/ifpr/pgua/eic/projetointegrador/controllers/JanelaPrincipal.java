10cac8e6ab8b04300be22023670be7a25cdc80d9
joanacabralmartins <joanacabralmartins@outlook.com>
2023-01-05 00:55:47 -0300

    implementação do método para realizar logoff

--- src/main/java/ifpr/pgua/eic/projetointegrador/controllers/JanelaPrincipal.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/controllers/JanelaPrincipal.java
@@ -1,9 +1,14 @@
 package ifpr.pgua.eic.projetointegrador.controllers;
 
+import java.util.Optional;
+
 import ifpr.pgua.eic.projetointegrador.App;
 import ifpr.pgua.eic.projetointegrador.utils.BorderPaneRegion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class JanelaPrincipal extends BaseController {
 
@@ -15,6 +20,20 @@ public class JanelaPrincipal extends BaseController {
     @FXML
     private void carregaTelaUsuario(ActionEvent evento) {
         App.changeScreenRegion("USUARIO", BorderPaneRegion.CENTER);
+    }
+
+    @FXML
+    private void deslogar(ActionEvent evento) {
+        Alert alert = new Alert(AlertType.CONFIRMATION, "Tem certeza que deseja sair?");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            carregaTelaLogin();
+        }
+    }
+
+    private void carregaTelaLogin() {
+        App.pushScreen("LOGIN");
     }
     
 }
--- src/main/java/ifpr/pgua/eic/projetointegrador/controllers/JanelaPrincipalCaroneiro.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/controllers/JanelaPrincipalCaroneiro.java
@@ -1,15 +1,34 @@
 package ifpr.pgua.eic.projetointegrador.controllers;
 
+import java.util.Optional;
+
 import ifpr.pgua.eic.projetointegrador.App;
 import ifpr.pgua.eic.projetointegrador.utils.BorderPaneRegion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class JanelaPrincipalCaroneiro extends BaseController {
 
     @FXML
     private void carregaTelaUsuario(ActionEvent evento) {
         App.changeScreenRegion("USUARIO", BorderPaneRegion.CENTER);
+    }
+
+    @FXML
+    private void deslogar(ActionEvent evento) {
+        Alert alert = new Alert(AlertType.CONFIRMATION, "Tem certeza que deseja sair?");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            carregaTelaLogin();
+        }
+    }
+
+    private void carregaTelaLogin() {
+        App.pushScreen("LOGIN");
     }
     
 }
